4e89a364110f80850c9e4ba5c58c83660c6f71bc
Mauricio Mendoza <160289979+JMauricioMendoza@users.noreply.github.com>
2025-04-30 10:29:21 -0600

    Agregar color institucional a configuración de Tailwind
    Se añadió la variable de color "institucional" al archivo de configuración de Tailwind, permitiendo su uso en todo el proyecto.

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -1,15 +1,18 @@
 // tailwind.config.js
+
 const { heroui } = require("@heroui/react");
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    // ...
-    // make sure it's pointing to the ROOT node_module
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        institucional: "#9d2348",
+      },
+    },
   },
   darkMode: "class",
   plugins: [heroui()],
