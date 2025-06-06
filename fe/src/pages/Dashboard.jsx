98eed6163c5118644c05a995689dd1d0644b6e76
Mauricio Mendoza <160289979+JMauricioMendoza@users.noreply.github.com>
2025-06-06 13:03:42 -0600

    Oculta menú del Dashboard a usuarios no admins
    Se ajusta la vista del Dashboard para ocultar elementos del menú que solo deben ser accesibles para usuarios administradores.

--- fe/src/pages/Dashboard.jsx
+++ fe/src/pages/Dashboard.jsx
@@ -12,50 +12,53 @@ import Layout from "../components/Layout";
 function Dashboard() {
   const [usuario, setUsuario] = useState(null);
 
+  const admin = sessionStorage.getItem("admin");
+
   return (
     <Layout usuario={usuario} setUsuario={setUsuario}>
       <div className="grid grid-cols-2 gap-5 mx-12 w-full">
-        <Card>
-          <CardHeader>
-            <h2 className="pt-2 pl-5 text-institucional text-lg font-semibold">
-              Administración
-            </h2>
-          </CardHeader>
-          <Divider />
-          <CardBody>
-            <div className="flex flex-col gap-2 pb-2 pl-5">
-              <HeroLink
-                as={RouterLink}
-                to="/tickets-todos"
-                className="font-medium"
-              >
-                Usuarios
-              </HeroLink>
-              <HeroLink
-                as={RouterLink}
-                to="/crear-ticket"
-                className="font-medium"
-              >
-                Departamentos
-              </HeroLink>
-              <HeroLink
-                as={RouterLink}
-                to="/crear-ticket"
-                className="font-medium"
-              >
-                Estatus de tickets
-              </HeroLink>
-              <HeroLink
-                as={RouterLink}
-                to="/crear-ticket"
-                className="font-medium"
-              >
-                Áreas de soporte
-              </HeroLink>
-            </div>
-          </CardBody>
-        </Card>
-
+        {admin === "true" && (
+          <Card>
+            <CardHeader>
+              <h2 className="pt-2 pl-5 text-institucional text-lg font-semibold">
+                Administración
+              </h2>
+            </CardHeader>
+            <Divider />
+            <CardBody>
+              <div className="flex flex-col gap-2 pb-2 pl-5">
+                <HeroLink
+                  as={RouterLink}
+                  to="/usuarios-todos"
+                  className="font-medium"
+                >
+                  Usuarios
+                </HeroLink>
+                <HeroLink
+                  as={RouterLink}
+                  to="/crear-ticket"
+                  className="font-medium"
+                >
+                  Departamentos
+                </HeroLink>
+                <HeroLink
+                  as={RouterLink}
+                  to="/crear-ticket"
+                  className="font-medium"
+                >
+                  Estatus de tickets
+                </HeroLink>
+                <HeroLink
+                  as={RouterLink}
+                  to="/crear-ticket"
+                  className="font-medium"
+                >
+                  Áreas de soporte
+                </HeroLink>
+              </div>
+            </CardBody>
+          </Card>
+        )}
         <Card>
           <CardHeader>
             <h2 className="pt-2 pl-5 text-institucional text-lg font-semibold">
